ce2c81d76154dedce346b2613efe1e906a1ec1f5
dezee_ <90754841+dezeeonpub@users.noreply.github.com>
2023-04-11 13:38:49 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,8 @@
+<head><link href="style.css" rel="stylesheet" type="text/css"></head>
+<footer>
+    <center><p>&copy; create by Muhammad Azizul Dzikri</p></center>
+</footer>
+</div>
+</body>
+
+</html>
